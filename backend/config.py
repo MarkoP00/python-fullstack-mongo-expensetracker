85e1e99376313bdffb0ed81aaafd202f136fc862
MarkoP00 <marebrat789@gmail.com>
2025-07-31 13:35:53 +0200

    Fixing config.py CORS

--- backend/config.py
+++ backend/config.py
@@ -10,7 +10,7 @@ load_dotenv()
 app = Flask(__name__)
 
 # My CORS settings
-CORS(app, origins=['http://localhost:5173', "https://python-fullstack-mongo-expensetrack.vercel.app/"], supports_credentials=True,
+CORS(app, origins=['http://localhost:5173', "https://python-fullstack-mongo-expensetrack.vercel.app"], supports_credentials=True,
      methods=["GET", "POST", "PATCH", "DELETE"], allow_headers=["Content-Type", "Authorization"])
 
 app.config["MONGO_URI"] = os.getenv("MONGODB_URL")
